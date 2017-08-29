69d0778dea13f648a6cadcc0979a2871fdb12078
Geethanjali53 <31436392+Geethanjali53@users.noreply.github.com>
2017-08-29 15:27:41 +0100

    first commit in to first project

---
+++ src/com.practice/HelloGeetha.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class HelloGeetha {
+    //entry point to tests
+    public static void main(String[] args) throws Throwable {
+
+        System.out.println("Hello Geetha");
+    }
+}
